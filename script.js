1a8efa18270358e23b2a6ebf605e52dc6302aec3
Alby <alby__1@hotmail.com>
2015-10-13 19:39:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,18 @@
 
 (function () {
   
+  var done = false;
+  
+  if (!done) {
   $(document).ajaxComplete(function(event,request, settings){
    $("#select2.select2-hidden-accessible").val(["200"]);
     $(".select2-selection__rendered").append(
     '<li title="Todos los jugadores" class="select2-selection__choice"><span class="select2-selection__choice__remove" role="presentation">×</span>Todos los jugadores</li>'
       );
+    if($(".select2-selection__rendered .select2-selection__choice").length) {
+      done= true;
+    }
 });
+  }
 
   })();
